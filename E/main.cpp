cf21b3ff094b51f66224333621bacd0ee255b77d
kekland <kekland@github.com>
2017-11-23 21:05:54 +0600

    Created thresholds finding algorithm for E

--- E/main.cpp
+++ E/main.cpp
@@ -3,52 +3,73 @@
 using namespace std;
 
 int main() {
-  freopen("input1.dat", "r", stdin);
+  freopen("input3.dat", "r", stdin);
   //freopen("main.out", "w", stdout);
 
-  int channelCount;
-  int messageLength;
-  int maximumErrorCount;
-  double messageTransmitChance;
-  double energyRequired[100];
-  int signalLevels;
-  int signals[100];
-  double signalProbability[100][100];
-  int length;
-  int dataWithError[100][1000];
+  int n = 5; //Number of channels
+  int l = 50; //Length of message
+  int e = 2; //Error count
+  double signalPercent = 0.6; //Percent of signal
+  double energy[100]; //Energy required
+  int lv = 15; //Maximum levels
+  double a[100][100]; //Probabilities for numbers in signal
+  int nl = 1000; //Noise length
+  double b[100][100]; //Probabilities for errors in signal
 
-  cin >> channelCount;
-  cin >> messageLength;
-  cin >> maximumErrorCount;
-  cin >> messageTransmitChance;
+  int thres[100]; //Thresholds for each channel
 
-  for(int i = 0; i < channelCount; i++) {
-    cin >> energyRequired[i];
+  /* INPUTS */
+  cin >> n >> l >> e >> signalPercent;
+
+  for(int i = 0; i < n; i++) {
+    cin >> energy[i];
   }
 
-  cin >> signalLevels;
-  for(int i = 0; i < signalLevels; i++) {
-    cin >> signals[i];
+  cin >> lv;
+  for(int i = 0; i < lv; i++) {
+    int x; cin >> x;
   }
 
-  for(int i = 0; i < channelCount; i++) {
-    for(int j = 0; j < signalLevels; j++) {
-      cin >> signalProbability[i][j];
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < lv; j++) {
+      cin >> a[i][j];
     }
   }
 
-  cin >> length;
-
-  for(int i = 0; i < channelCount; i++) {
-    for(int j = 0; j < length; j++) {
-      cin >> dataWithError[i][j];
+  cin >> nl;
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < nl; j++) {
+      int noise;
+      cin >> noise;
+      b[i][noise] += 0.001;
     }
   }
 
-  for(int i = 0; i < channelCount; i++) {
-    for(int j = 0; j < 10; j++) {
-      cout << dataWithError[i][j] << " ";
+  /* FINDING THRESHOLDS */
+  for(int ch = 0; ch < n; ch++) {
+    double bestR = 10000.0;
+    int bestN = 0;
+    for(int m = 0; m < lv; m++) {
+      double r = 0.0, k = 0.0, q = 0.0;
+      for(int i = m; i < lv; i++) {
+        k += b[ch][i];
+      }
+      for(int i = 0; i < m; i++) {
+        double temp = 0.0;
+        for(int j = 0; i + j < m; j++) {
+          temp += a[ch][j];
+        }
+        q += b[ch][i] * temp;
+      }
+
+      r = k + q;
+      if(bestR > r) {
+        bestR = r;
+        bestN = m;
+      }
     }
-    cout << endl;
+
+    thres[ch] = bestN;
   }
+  
 }
---
+++ EPython/main.py
@@ -0,0 +1,12 @@
+channelCount = 5
+messageLength = 50
+errorCount = 2
+messageTransmitPercent = 0.6
+energySpent = []
+signalLevels = 15
+levels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+def readInput():
+    lines = input().split('\n')
+
+    
